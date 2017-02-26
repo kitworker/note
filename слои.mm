2852b243a888df62e1088191fa08279090eb9477
kitworker <kasyanov@gmail.com>
2017-02-26 22:03:47 +0300

    less parallel struct

--- слои.mm
+++ слои.mm
@@ -1,10 +1,55 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1488133211398" LINK="&#x441;&#x442;&#x430;&#x442;&#x44c;&#x44f;.mm" MODIFIED="1488134935230" TEXT="&#x441;&#x43b;&#x43e;&#x438;(&#x43e;&#x431;&#x44a;&#x435;&#x43a;&#x442; &#x443;&#x43f;&#x440;&#x430;&#x432;&#x43b;&#x435;&#x43d;&#x438;&#x44f;)">
-<node CREATED="1488132442518" MODIFIED="1488132606020" POSITION="left" TEXT="&#x443;&#x43d;&#x438;&#x432;&#x435;&#x440;&#x441;&#x430;&#x43b;&#x44c;&#x43d;&#x44b;&#x439; &#x432;&#x435;&#x440;&#x445;&#x43d;&#x438;&#x439;"/>
-<node CREATED="1488132606822" FOLDED="true" MODIFIED="1488133221056" POSITION="left" TEXT="&#x440;&#x435;&#x430;&#x43b;&#x438;&#x437;&#x430;&#x446;&#x438;&#x44f;">
-<node CREATED="1488132690390" MODIFIED="1488132700848" TEXT="&#x43f;&#x440;&#x435;&#x434;&#x441;&#x442;&#x430;&#x432;&#x43b;&#x435;&#x43d;&#x438;&#x435;"/>
+<node CREATED="1488132267150" ID="ID_186490171" LINK="../MonoGlyph_realize.mm" MODIFIED="1488133211300" TEXT="&#x441;&#x43b;&#x43e;&#x438;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      new: metrica
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="idea"/>
+<node COLOR="#ff0000" CREATED="1488132442518" ID="ID_1190061231" MODIFIED="1488134283631" POSITION="left" TEXT="&#x443;&#x43d;&#x438;&#x432;&#x435;&#x440;&#x441;&#x430;&#x43b;&#x44c;&#x43d;&#x44b;&#x439; &#x432;&#x435;&#x440;&#x445;&#x43d;&#x438;&#x439;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1082;&#1086;&#1084;&#1087;&#1086;&#1079;&#1080;&#1094;&#1080;&#1103; &#1080; &#1076;&#1077;&#1082;&#1086;&#1084;&#1087;&#1086;&#1079;&#1080;&#1094;&#1080;&#1103;
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#990000" CREATED="1488133273199" ID="ID_1230961923" LINK="&#x440;&#x435;&#x430;&#x43b;&#x438;&#x437;&#x430;&#x446;&#x438;&#x44f;.mm" MODIFIED="1488134287886" TEXT="&#x440;&#x435;&#x430;&#x43b;&#x438;&#x437;&#x430;&#x446;&#x438;&#x44f;">
+<node COLOR="#338800" CREATED="1488132706934" ID="ID_1361242079" MODIFIED="1488134290085" TEXT="&#x43b;&#x43e;&#x433;&#x438;&#x43a;&#x430;"/>
 </node>
-<node CREATED="1488132706934" MODIFIED="1488133061750" POSITION="left" TEXT="&#x43b;&#x43e;&#x433;&#x438;&#x43a;&#x430;"/>
+</node>
+<node CREATED="1488133215509" ID="ID_1593980797" MODIFIED="1488133312730" POSITION="right" TEXT="&#x43c;&#x435;&#x442;&#x440;&#x438;&#x43a;&#x430; ">
+<node CREATED="1488133312732" ID="ID_1394256979" MODIFIED="1488135516353" TEXT="&#x436;&#x43e;&#x441;&#x442;&#x43a;&#x43e;&#x435; &#x43a;&#x43e;&#x434;&#x438;&#x440;&#x43e;&#x432;&#x430;&#x43d;&#x438;&#x435; &#x43a;&#x43e;&#x434;&#x438;&#x440;&#x43e;&#x432;&#x430;&#x43d;&#x438;&#x435;">
+<node CREATED="1488133505861" ID="ID_1297055701" MODIFIED="1488133514749" TEXT="&#x434;&#x435;&#x448;&#x438;&#x444;&#x440;&#x430;&#x442;&#x43e;&#x440;">
+<node CREATED="1488133524462" ID="ID_589925477" MODIFIED="1488135626898" TEXT="&#x43f;&#x430;&#x442;&#x442;&#x435;&#x440;&#x43d; Visitor">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1086;&#1090;&#1082;&#1072;&#1079; &#1086;&#1090; &#1087;&#1072;&#1088;&#1072;&#1083;&#1083;&#1077;&#1083;&#1100;&#1085;&#1086;&#1081; &#1089;&#1090;&#1088;&#1091;&#1082;&#1090;&#1091;&#1088;&#1099;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1488133542822" ID="ID_780102672" MODIFIED="1488133542822" TEXT=""/>
+</node>
+</node>
+</node>
+<node CREATED="1488134314933" ID="ID_192296532" MODIFIED="1488134344946" POSITION="right" TEXT="&#x43f;&#x43e;&#x43b;&#x438;&#x43c;&#x43e;&#x440;&#x444;&#x438;&#x437;&#x43c; &#x43a;&#x430;&#x43a; &#x441;&#x440;&#x435;&#x434;&#x441;&#x442;&#x432;&#x43e; &#x441;&#x43a;&#x432;&#x43e;&#x437;&#x43d;&#x43e;&#x439; &#x440;&#x430;&#x437;&#x440;&#x430;&#x431;&#x43e;&#x442;&#x43a;&#x438;"/>
 </node>
 </map>
